01d55dfe3ce8eff6b5b459c4b14a903541ccac33
danielyosef <danielyosef92@gmail.com>
2016-12-04 20:07:37 +0200

    update with birthday class

--- app/src/main/java/com/example/danielyosef/androidapps/MainActivity.java
+++ app/src/main/java/com/example/danielyosef/androidapps/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -17,17 +18,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView myList = (ListView)findViewById(R.id.myList);
+        final ListView myList = (ListView)findViewById(R.id.myList);
 
-        final ArrayList<Screen> IntentArray = new ArrayList<Screen>();
+        Screen[] array = new Screen[] {
+                new Screen("calc", Calc.class),
+                new Screen("constrain1", Constrain1.class),
+                new Screen("constrain2", Constrain2.class),
+                new Screen("constrain3", Constrain3.class),
+                new Screen("mybirthday", MyBirthdays.class)
+        };
 
-        IntentArray.add(new Screen("calc",new Intent(getApplicationContext(),Calc.class)));
-        IntentArray.add(new Screen("constrain1",new Intent(getApplicationContext(),Constrain1.class)));
-        IntentArray.add(new Screen("constrain2",new Intent(getApplicationContext(),Constrain2.class)));
-        IntentArray.add(new Screen("constrain3",new Intent(getApplicationContext(),Constrain3.class)));
-
-
-        ArrayAdapter ad = new ArrayAdapter(this, android.R.layout.simple_list_item_1,IntentArray);
+        ArrayAdapter<Screen> ad = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,array);
 
         myList.setAdapter(ad);
 
@@ -35,7 +36,13 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                startActivity(IntentArray.get(position).getI());
+                Screen clicked = (Screen) myList.getItemAtPosition(position);
+                if(clicked.c == null){
+                    Toast.makeText(getBaseContext(), "Activity missing", Toast.LENGTH_LONG).show();
+                }else {
+                    Intent i = new Intent(getBaseContext(), clicked.c);
+                    startActivity(i);
+                }
             }
 
         });
@@ -47,19 +54,19 @@ public class MainActivity extends AppCompatActivity {
 class Screen {
 
     String s;
-    Intent i;
+    Class c;
 
-    public Screen(String s, Intent i) {
+    public Screen(String s, Class i) {
         this.s = s;
-        this.i = i;
+        this.c = i;
     }
 
-    public Intent getI() {
-        return i;
+    public Class getI() {
+        return c;
     }
 
-    public void setI(Intent i) {
-        this.i = i;
+    public void setI(Class i) {
+        this.c = i;
     }
 
     public String getS() {
